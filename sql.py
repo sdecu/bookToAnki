b7c3f26f9029838a21506c13208885b34b454acb
Chris <sdecuir12@gmail.com>
2024-09-23 15:36:27 -0500

    add function to query common french words from database

--- sql.py
+++ sql.py
@@ -41,23 +41,42 @@ def query(db, cols, vals):
     except sqlite3.Error as error:
         print('Error occured - ', error)
 
-def create_book_db(bookname):
+def read_common_words_fr():
+    try:
+        with sqlite3.connect('common.db') as cnt:
+            cursor = cnt.cursor()
+
+            cursor.execute('SELECT ngram FROM common_words_fr LIMIT 1000')
+            words = cursor.fetchall()
+            return words
+
+    except sqlite3.Error as error:
+        print('Error occured - ', error)
+    
+    finally:
+        if cursor:
+            cursor.close()
+            
+
+
+
+def create_book_db(bookname, dictionary):
     try:
         with sqlite3.connect('common.db') as cnt:
             print('db init')
             
-            cnt.execute(f'DROP TABLE IF EXISTS {bookname}')
+            cnt.execute('DROP TABLE IF EXISTS ?', (bookname))
 
-            cnt.execute(f'''CREATE TABLE IF NOT EXISTS {bookname}
+            cnt.execute('''CREATE TABLE IF NOT EXISTS  ?
             (WORD TEXT PRIMARY KEY,
             SENTENCE TEXT,
             TRANSWORD TEXT,
-            TRANSSENTENCE TEXT)''')
+            TRANSSENTENCE TEXT)''', (bookname))
 
             cursor = cnt.cursor()
             
 
-            for word, sent in dict.items():
+            for word, sent in dictionary.items():
                 cursor.execute('INSERT INTO common_words_fr (WORD, SENTENCE, TRANSWORD, TRANSSENTENCE) VALUES (?, ?, ?, ?)',
                                 (word, sent, translate_text('en', word), translate_text('en', sent)))
             print('success')
