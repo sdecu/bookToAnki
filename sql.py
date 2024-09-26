670ae4aab326a533cdd437e47306bc2a473cb72b
Chris <sdecuir12@gmail.com>
2024-09-26 16:59:56 -0500

    add function to add words to known words

--- sql.py
+++ sql.py
@@ -48,17 +48,12 @@ def read_common_words_fr():
 
             cursor.execute('SELECT ngram FROM common_words_fr LIMIT 1000')
             words = cursor.fetchall()
+            cursor.close()
             return words
 
     except sqlite3.Error as error:
         print('Error occured - ', error)
     
-    finally:
-        if cursor:
-            cursor.close()
-            
-
-
 
 def create_book_db(bookname, dictionary):
     try:
@@ -68,22 +63,39 @@ def create_book_db(bookname, dictionary):
             cnt.execute('DROP TABLE IF EXISTS ?', (bookname))
 
             cnt.execute('''CREATE TABLE IF NOT EXISTS  ?
-            (WORD TEXT PRIMARY KEY,
-            SENTENCE TEXT,
-            TRANSWORD TEXT,
-            TRANSSENTENCE TEXT)''', (bookname))
-
+            (word TEXT PRIMARY KEY,
+            count INT)''', (bookname))
             cursor = cnt.cursor()
             
 
             for word, sent in dictionary.items():
-                cursor.execute('INSERT INTO common_words_fr (WORD, SENTENCE, TRANSWORD, TRANSSENTENCE) VALUES (?, ?, ?, ?)',
-                                (word, sent, translate_text('en', word), translate_text('en', sent)))
+                cursor.execute('INSERT INTO ? (WORD, SENTENCE) VALUES (?, ?)',
+                                (bookname, word, sent))
+            cursor.close()
             print('success')
 
     except sqlite3.Error as error:
         print('Error occured - ', error)
     
+def add_book_words_to_known_words(book_table, freq):
+    try:
+        with sqlite3.connect('common.db') as cnt:
+            print('db init')
+            
+            cnt.execute('''CREATE TABLE IF NOT EXISTS  ?
+            (WORD TEXT PRIMARY KEY)''', "known_words")
+
+            cursor = cnt.cursor()
+            words = cursor.execute('SELECT word FROM ? WHERE count > ?', (book_table, freq)) 
+
+            for word in cursor.fetchall():
+                cursor.execute('INSERT INTO known_words (word) VALUES (?)',
+                                (word))
+            cursor.close()
+            print('success')
+
+    except sqlite3.Error as error:
+        print('Error occured - ', error)
     
 if __name__ == "__main__":
     main()
