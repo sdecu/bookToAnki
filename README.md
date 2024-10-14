bab519e52386e69f70e03ec8155b21871ab744f9
Chris <sdecuir12@gmail.com>
2024-10-13 22:54:25 -0500

    fix database

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # bookToAnki
 convert a book to an anki deck based off the most common words
+go to google console
+create a new project
+select project
+go to api & services
+search for and enable "Cloud Translation API" after clicking "Library"
+Select Credentials
+Select service accounts
+name it "booktoanki"
+select "Cloud Translation API Admin"
+
+install google cloud cli https://cloud.google.com/sdk/docs/install#deb
+run gcloud init and select your project with the api enabled
+run gcloud auth application-default login
+
--- deckMaker.py
+++ deckMaker.py
@@ -2,6 +2,45 @@ import genanki
 import json
 import click
 
+import os
+
+from google.cloud import translate_v3
+
+PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")
+
+
+# Initialize Translation client
+def batch_translate_to_english(
+    text: str = "YOUR_TEXT_TO_TRANSLATE",
+    language_code: str = "fr",
+) -> translate_v3.TranslateTextResponse:
+    """Translating Text from English.
+    Args:
+        text: The content to translate.
+        language_code: The language code for the translation.
+            E.g. "fr" for French, "es" for Spanish, etc.
+            Available languages: https://cloud.google.com/translate/docs/languages#neural_machine_translation_model
+    """
+
+    client = translate_v3.TranslationServiceClient()
+    parent = f"projects/{PROJECT_ID}/locations/global"
+    # Translate text from English to chosen language
+    # Supported mime types: # https://cloud.google.com/translate/docs/supported-formats
+    response = client.translate_text(
+        contents=[text],
+        target_language_code=language_code,
+        parent=parent,
+        mime_type="text/plain",
+        source_language_code="en-US",
+    )
+
+    # Display the translation for each input text provided
+    for translation in response.translations:
+        print(f"Translated text: {translation.translated_text}")
+    # Example response:
+    # Translated text: Bonjour comment vas-tu aujourd'hui?
+
+    return response
 def translate_text(target: str, text: str):
     """Translates text into the target language.
 
